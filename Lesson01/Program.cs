1ddbc00c9807ac70abead58dad15733b7916153f
Nikolay_Yakushin <ny.phkp@gmail.com>
2022-03-07 19:07:15 +0300

    first excercise done done

--- Lesson01/Program.cs
+++ Lesson01/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.WriteLine("Введите целое число");
 int value = Convert.ToInt32(Console.ReadLine());
-int kvadrat_value = value * value
+int kvadrat_value = value * value;
 Console.Write("Квадрат числа равен ");
 Console.WriteLine(kvadrat_value);
 
